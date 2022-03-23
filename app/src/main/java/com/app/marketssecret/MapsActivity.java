8e9b5342451b4be0ae370e6992b91539274511e0
Artem Boytunov <64827737+ArtemBoytunov@users.noreply.github.com>
2022-03-24 02:18:59 +0900

    Initial commit

---
+++ app/src/main/java/com/app/marketssecret/MapsActivity.java
@@ -0,0 +1,337 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.app.marketssecret;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private static final String TAG = "MapsActivity";
+    ListView lstPlaces;
+    private PlacesClient mPlacesClient;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
+
+    private Location mLastKnownLocation;
+
+
+    private final LatLng mDefaultLocation = new LatLng(62.039447564252754, 129.74317940827348);
+    private static final int DEFAULT_ZOOM = 15;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean mLocationPermissionGranted;
+
+    private static final int M_MAX_ENTRIES = 5;
+    private String[] mLikelyPlaceNames;
+    private String[] mLikelyPlaceAddresses;
+    private String[] mLikelyPlaceAttributions;
+    private LatLng[] mLikelyPlaceLatLngs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        String apiKey = getString(R.string.google_maps_key);
+        Places.initialize(getApplicationContext(), apiKey);
+        mPlacesClient = Places.createClient(this);
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.action_geolocate:
+
+                pickCurrentPlace();
+                return true;
+
+            default:
+
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+
+    private void getLocationPermission() {
+
+        mLocationPermissionGranted = false;
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng ozon = new LatLng(62.035268741956, 129.7515914379489);
+        mMap.addMarker(new MarkerOptions().position(ozon).title("ozon"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ozon));
+
+
+        LatLng itpark = new LatLng(62.032634691380544, 129.75060230831093);
+        mMap.addMarker(new MarkerOptions().position(itpark).title("itpark"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(itpark));
+
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        getLocationPermission();
+
+    }
+
+
+    private void getCurrentPlaceLikelihoods() {
+        List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME, Place.Field.ADDRESS,
+                Place.Field.LAT_LNG);
+
+
+        @SuppressWarnings("MissingPermission") final FindCurrentPlaceRequest request =
+                FindCurrentPlaceRequest.builder(placeFields).build();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        Task<FindCurrentPlaceResponse> placeResponse = mPlacesClient.findCurrentPlace(request);
+        placeResponse.addOnCompleteListener(this,
+                new OnCompleteListener<FindCurrentPlaceResponse>() {
+                    @Override
+                    public void onComplete(@NonNull Task<FindCurrentPlaceResponse> task) {
+                        if (task.isSuccessful()) {
+                            FindCurrentPlaceResponse response = task.getResult();
+                            int count;
+                            if (response.getPlaceLikelihoods().size() < M_MAX_ENTRIES) {
+                                count = response.getPlaceLikelihoods().size();
+                            } else {
+                                count = M_MAX_ENTRIES;
+                            }
+
+                            int i = 0;
+                            mLikelyPlaceNames = new String[count];
+                            mLikelyPlaceAddresses = new String[count];
+                            mLikelyPlaceAttributions = new String[count];
+                            mLikelyPlaceLatLngs = new LatLng[count];
+
+                            for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
+                                Place currPlace = placeLikelihood.getPlace();
+                                mLikelyPlaceNames[i] = currPlace.getName();
+                                mLikelyPlaceAddresses[i] = currPlace.getAddress();
+                                mLikelyPlaceAttributions[i] = (currPlace.getAttributions() == null) ?
+                                        null : TextUtils.join(" ", currPlace.getAttributions());
+                                mLikelyPlaceLatLngs[i] = currPlace.getLatLng();
+
+                                String currLatLng = (mLikelyPlaceLatLngs[i] == null) ?
+                                        "" : mLikelyPlaceLatLngs[i].toString();
+
+                                Log.i(TAG, String.format("Place " + currPlace.getName()
+                                        + " has likelihood: " + placeLikelihood.getLikelihood()
+                                        + " at " + currLatLng));
+
+                                i++;
+                                if (i > (count - 1)) {
+                                    break;
+                                }
+                            }
+
+
+                            fillPlacesList();
+                        } else {
+                            Exception exception = task.getException();
+                            if (exception instanceof ApiException) {
+                                ApiException apiException = (ApiException) exception;
+                                Log.e(TAG, "Place not found: " + apiException.getStatusCode());
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (mLocationPermissionGranted) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                    return;
+                }
+                Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+
+                            mLastKnownLocation = location;
+                            Log.d(TAG, "Latitude: " + mLastKnownLocation.getLatitude());
+                            Log.d(TAG, "Longitude: " + mLastKnownLocation.getLongitude());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(mLastKnownLocation.getLatitude(),
+                                            mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            mMap.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+                        }
+
+                        getCurrentPlaceLikelihoods();
+                    }
+                });
+            }
+        } catch (Exception e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    /**
+     * Fetch a list of likely places, and show the current place on the map - provided the user
+     * has granted location permission.
+     */
+    private void pickCurrentPlace() {
+        if (mMap == null) {
+            return;
+        }
+
+        if (mLocationPermissionGranted) {
+            getDeviceLocation();
+        } else {
+
+            Log.i(TAG, "The user did not grant location permission.");
+
+
+            mMap.addMarker(new MarkerOptions()
+                    .title(getString(R.string.default_info_title))
+                    .position(mDefaultLocation)
+                    .snippet(getString(R.string.default_info_snippet)));
+
+
+            getLocationPermission();
+        }
+    }
+
+
+    private AdapterView.OnItemClickListener listClickedHandler = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView parent, View v, int position, long id) {
+            // position will give us the index of which place was selected in the array
+            LatLng markerLatLng = mLikelyPlaceLatLngs[position];
+            String markerSnippet = mLikelyPlaceAddresses[position];
+            if (mLikelyPlaceAttributions[position] != null) {
+                markerSnippet = markerSnippet + "\n" + mLikelyPlaceAttributions[position];
+            }
+
+
+            mMap.addMarker(new MarkerOptions()
+                    .title(mLikelyPlaceNames[position])
+                    .position(markerLatLng)
+                    .snippet(markerSnippet));
+
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(markerLatLng));
+        }
+    };
+
+
+    private void fillPlacesList() {
+
+        ArrayAdapter<String> placesAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mLikelyPlaceNames);
+        lstPlaces.setAdapter(placesAdapter);
+        lstPlaces.setOnItemClickListener(listClickedHandler);
+    }
+
+}
